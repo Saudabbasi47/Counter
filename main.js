4fbd8be0cd5518890c901072f5ab5a5cd751cbb9
Saudabbasi47 <saudabbasi22033@gmail.com>
2024-11-22 16:23:01 +0500

    Updated UI and Bug Fixed

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 let count = 0;
-function increase(){
-    count++;
-    let num= document.getElementById('num');
-    num.innerText = count;
+function increase() {
+  count++;
+  let num = document.getElementById("num");
+  num.innerText = count;
 }
-function decrease(){
-    count--;
-  
-    num.innerText = count;
+function reset() {
+  count = 0;
+  num.innerText = count;
+}
+function decrease() {
+  count--;
 
-}
-function reset(){
-    return num.innerText = 0;
+  num.innerText = count;
 }
